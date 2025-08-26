4ce81526b9154824453485c96e0296b2a070e1ab
Zainab Choudhry <zainabchoudhry05@gmail.com>
2025-08-26 16:29:58 +0100

    completed main setup

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+//Load environment variable
+import { config } from "dotenv";
+config();
+
+// Import express and cors
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+
+// Set up express
+const app = express();
+app.disable("x-powered-by");
+app.use(cors());
+// Tell express to use a JSON parser middleware
+app.use(express.json());
+// Tell express to use a URL Encoding middleware
+app.use(express.urlencoded({ extended: true }));
+
+//Add routers below:
+
+
+//Middleware
+
+
+// Test route to check if server is running 
+// Test route to check if the server is running
+app.get("/test", async (req, res) => {
+  res.send("ok");
+});
+// Start API server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`\n Server is running on http://localhost:${PORT}\n`);
+});
